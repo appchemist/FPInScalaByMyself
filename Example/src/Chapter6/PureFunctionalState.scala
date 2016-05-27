f65f460b91ab04a740b936493bb72f248a4cd708
appchemist <dct012@gmail.com>
2016-05-27 13:52:05 +0900

    Chapter6 연습 문제(6.6)

---
+++ Example/src/Chapter6/PureFunctionalState.scala
@@ -0,0 +1,118 @@
+package Chapter6
+
+import scala.List._;
+import scala.::;
+
+/**
+  * Created by appchemist on 2016. 5. 24..
+  */
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
+
+object RNG {
+  def randomPair(rng: RNG): ((Int, Int), RNG) = {
+    val (i1, rng2) = rng.nextInt
+    val (i2, rng3) = rng2.nextInt
+    ((i1, i2), rng3)
+  }
+
+  // 6.1
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i1, rng2) = rng.nextInt
+
+    if (i1 == Int.MinValue) (0, rng2)
+    else if (i1 < 0) (-i1, rng2)
+    else (i1, rng2)
+  }
+
+  // 6.2
+  def double(rng: RNG): (Double, RNG) = {
+    val (i1, rng2) = nonNegativeInt(rng)
+
+    (i1 / (Int.MaxValue.toDouble + 1), rng2)
+  }
+
+  // 6.3
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i1, rng2) = nonNegativeInt(rng)
+    val (d2, rng3) = double(rng2)
+
+    ((i1, d2), rng3)
+  }
+
+  // 6.3
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i, d), rng2) = intDouble(rng)
+
+    ((d, i), rng2)
+  }
+
+  // 6.3
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, rng2) = double(rng)
+    val (d2, rng3) = double(rng2)
+    val (d3, rng4) = double(rng3)
+
+    ((d1, d2, d3), rng4)
+  }
+
+  // 6.4
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count == 0) return (Nil, rng)
+    else {
+      val (i, rng2) = nonNegativeInt(rng)
+      val (list, rng3) = ints(count - 1)(rng2)
+      (i :: ints(count - 1)(rng2)._1, rng3)
+    }
+  }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt)(i=>i - i % 2)
+
+  // 6.5
+  def doubleViaMap: Rand[Double] =
+    map(nonNegativeInt)(i => i / Int.MaxValue.toDouble + 1)
+
+  // 6.6
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, rng2) = ra(rng)
+      val (b, rng3) = rb(rng2)
+
+      (f(a, b), rng3)
+    }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)((_, _))
+
+  val randIntDouble: Rand[(Int, Double)] =
+    both(int, double)
+
+  val randDoubleInt: Rnad[(Double, Int)] =
+    both(double, int)
+}
+
